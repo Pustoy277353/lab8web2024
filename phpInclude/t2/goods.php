8cc7e594d8c123172ccf56191db569631e0e2f3a
Pustoy277353 <98615042+Pustoy277353@users.noreply.github.com>
2024-04-15 06:01:35 +0300

    Update goods.php

--- phpInclude/t2/goods.php
+++ phpInclude/t2/goods.php
@@ -3,10 +3,3 @@
     Цена:<?php echo $product['price']; ?>
     <?php echo $product['description']; ?>
 </div>
-
-git init
-git add *
-git commit -m "init"
-git branch -M master
-git remote add origin https://github.com/Pustoy277353/lab8web2024.git
-git push -u origin master
